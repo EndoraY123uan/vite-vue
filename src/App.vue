b4d1ca622868c51280794ab19c0cd1ae82c9cdfe
yuanjinzi <jinzi.yuan@weimob.com>
2024-05-08 22:49:00 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>我是app</div>
+</template>
+
+<script setup lang="ts">
+// export default {
+//
+// }
+
+const a = () => {
+  console.log('hhahahahah');
+};
+
+a();
+</script>
+
+<style scoped></style>
